243eab92b2077a15552c869a80eb2d52d8f5bcab
bling-yshs <377178599@qq.com>
2024-10-11 15:38:00 +0800

    feat: 可以全局排除指定路径

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -5,18 +5,27 @@ import { createStore } from '@tauri-apps/plugin-store'
 export const useConfigStore = defineStore('config', () => {
   const theme = ref<string>('')
   const excludeExts = ref<Array<string>>([])
+  const excludePaths = ref<Array<string>>([])
+  const remindNum = ref<number>(0)
+  const enableGitignore = ref<boolean>(false)
 
-  watch(
-    theme,
-    async (value) => {
-      const dbStore = await createStore('code-merger-tauri.bin')
-      await dbStore.set('theme', value)
-      await dbStore.save()
-    },
-    {
-      deep: true
-    }
-  )
+  watch(enableGitignore, async (value) => {
+    const dbStore = await createStore('code-merger-tauri.bin')
+    await dbStore.set('enableGitignore', value)
+    await dbStore.save()
+  })
+
+  watch(remindNum, async (value) => {
+    const dbStore = await createStore('code-merger-tauri.bin')
+    await dbStore.set('remindNum', value)
+    await dbStore.save()
+  })
+
+  watch(theme, async (value) => {
+    const dbStore = await createStore('code-merger-tauri.bin')
+    await dbStore.set('theme', value)
+    await dbStore.save()
+  })
 
   watch(
     excludeExts,
@@ -29,7 +38,19 @@ export const useConfigStore = defineStore('config', () => {
       deep: true
     }
   )
-  return { theme, excludeExts }
+
+  watch(
+    excludePaths,
+    async (value) => {
+      const dbStore = await createStore('code-merger-tauri.bin')
+      await dbStore.set('excludePaths', value)
+      await dbStore.save()
+    },
+    {
+      deep: true
+    }
+  )
+  return { theme, excludeExts, excludePaths, remindNum, enableGitignore }
 })
 
 export const initConfigStore = async () => {
@@ -37,4 +58,7 @@ export const initConfigStore = async () => {
   const configStore = useConfigStore()
   configStore.theme = (await dbStore.get('theme')) || 'light'
   configStore.excludeExts = (await dbStore.get('excludeExts')) || new Array<string>()
+  configStore.excludePaths = (await dbStore.get('excludePaths')) || new Array<string>()
+  configStore.remindNum = (await dbStore.get('remindNum')) || 0
+  configStore.enableGitignore = (await dbStore.get('enableGitignore')) || false
 }
