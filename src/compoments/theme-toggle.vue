de84646f333bd1f8e42be105ebdbcc171b6d4923
bling-yshs <377178599@qq.com>
2024-10-11 20:46:53 +0800

    feat: 二次确认提示数

--- src/compoments/theme-toggle.vue
+++ src/compoments/theme-toggle.vue
@@ -1,30 +1,25 @@
 <template>
-  <el-button class="fixed bottom-20 right-20" circle @click="toggleDark">
-    <el-icon v-if="config.theme === 'dark'">
-      <Sunny />
-    </el-icon>
-    <el-icon v-if="config.theme === 'light'">
-      <Moon />
-    </el-icon>
-  </el-button>
+  <el-tooltip content='切换主题' placement="left">
+    <el-button class="fixed bottom-20 right-20" circle @click="toggleTheme">
+      <el-icon v-if="config.theme === 'dark'">
+        <Sunny />
+      </el-icon>
+      <el-icon v-if="config.theme === 'light'">
+        <Moon />
+      </el-icon>
+    </el-button>
+  </el-tooltip>
 </template>
 <script setup lang="ts">
 import { Moon, Sunny } from '@element-plus/icons-vue'
-import { useDark } from '@vueuse/core'
-import { onMounted } from 'vue'
 import { relaunch } from '@tauri-apps/plugin-process'
 import { ElMessageBox } from 'element-plus'
 import { useConfigStore } from '@/stores/config.ts'
 
 let config = useConfigStore()
 
-const isDark = useDark()
-
-onMounted(async () => {})
-
-const toggleDark = async () => {
-  isDark.value = !isDark.value
-  config.theme = isDark.value ? 'dark' : 'light'
+const toggleTheme = async () => {
+  config.toggleTheme()
   let b = await askRelaunch()
   if (!b) {
     return
